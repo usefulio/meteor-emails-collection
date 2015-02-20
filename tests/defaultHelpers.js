7b98b2f4f4cc15e671a05228410fd30f3edd95fa
Joshua Ohlman <codewriterohs@gmail.com>
2015-02-20 15:46:29 -0600

    Add prettyAddress helper

--- tests/defaultHelpers.js
+++ tests/defaultHelpers.js
@@ -17,3 +17,13 @@ testAndCleanup("Emails - default helpers - fromUser and toUser", function (test)
     , toId: Meteor.users.findOne({"profile.name": "sam"})._id
   });
 });
+
+testAndCleanup("Emails - default helpers - prettyAddress", function (test) {
+  Emails.routes.default.action = function (email) {
+    test.equal(this.get("prettyAddress", "joe@example.com", "Joe Schmuc"), "\"Joe Schmuc\" <joe@example.com>");
+
+    sent = email;
+  };
+
+  Emails.send({}); 
+});
