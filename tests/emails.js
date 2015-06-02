4cb942f7527f1d4fb202113c414efc0940862733
Joshua Ohlman <codewriterohs@gmail.com>
2015-06-01 20:50:30 -0500

    Add afterSend hook

--- tests/emails.js
+++ tests/emails.js
@@ -61,3 +61,27 @@ Tinytest.add("Emails - supports overriding fields", function (test) {
   test.equal(email.html, '<p>this is a test of the cwohlman:emails package</p>');
   test.equal(email.customField, true);
 });
+
+Tinytest.add("Emails - calls after send", function (test) {
+  Emails.route('test', {
+    afterSend: function (email) {
+      sent = true;
+    }
+  });
+
+  var sent = false;
+  var email = Emails.send('test', {
+    to: 'joe@example.com'
+    , from: 'admin@example.com'
+    , subject: 'test'
+    , text: 'this is a test of the cwohlman:emails package'
+    , html: '<p>this is a test of the cwohlman:emails package</p>'
+  });
+
+  test.equal(sent, true);
+  test.equal(email.to, 'joe@example.com');
+  test.equal(email.from, 'admin@example.com');
+  test.equal(email.subject, 'test');
+  test.equal(email.text, 'this is a test of the cwohlman:emails package');
+  test.equal(email.html, '<p>this is a test of the cwohlman:emails package</p>');
+});
